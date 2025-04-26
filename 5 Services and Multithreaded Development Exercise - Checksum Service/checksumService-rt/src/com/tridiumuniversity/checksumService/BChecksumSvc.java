1c74e49133057faeda118309ae9c08d4960c08a2
gerrymurphysse <gerard.murphy@sse.com>
2025-04-27 00:27:36 +0100

    Exercise code for comment, updated with Exercise 5 checksumService

--- 5 Services and Multithreaded Development Exercise - Checksum Service/checksumService-rt/src/com/tridiumuniversity/checksumService/BChecksumSvc.java
+++ 5 Services and Multithreaded Development Exercise - Checksum Service/checksumService-rt/src/com/tridiumuniversity/checksumService/BChecksumSvc.java
@@ -1,121 +1,149 @@
 package com.tridiumuniversity.checksumService;
 
+import javax.baja.control.trigger.BTimeTrigger;
 import javax.baja.nre.annotations.NiagaraAction;
 import javax.baja.nre.annotations.NiagaraProperty;
 import javax.baja.nre.annotations.NiagaraType;
 import javax.baja.sys.*;
+import javax.baja.util.BIRestrictedComponent;
 import java.nio.charset.StandardCharsets;
+import java.util.concurrent.ExecutorService;
 import java.util.logging.Logger;
+import java.util.zip.CRC32;
+import java.util.zip.Checksum;
+
+import static java.util.concurrent.Executors.newSingleThreadExecutor;
 
 @NiagaraType
-@NiagaraProperty(
-        name = "checksum",
-        type = "String",
-        defaultValue = ""
-)
-@NiagaraAction(
-        name = "generateChecksum",
-        flags = Flags.SUMMARY
-)
-public class BChecksumSvc extends BAbstractService {
-//region /*+ ------------ BEGIN BAJA AUTO GENERATED CODE ------------ +*/
+@NiagaraProperty(name = "checksum",type = "BString",defaultValue = "")
+@NiagaraProperty(name="trigger",type = "BTimeTrigger",defaultValue = "new BTimeTrigger()")
+@NiagaraAction(name="generateChecksum", flags = Flags.HIDDEN)
+public class BChecksumSvc extends BAbstractService implements BIRestrictedComponent {
+
+    //region /*+ ------------ BEGIN BAJA AUTO GENERATED CODE ------------ +*/
 //@formatter:off
-/*@ $com.tridiumuniversity.checksumService.BChecksumSvc(822350172)1.0$ @*/
-/* Generated Fri Apr 25 15:25:46 BST 2025 by Slot-o-Matic (c) Tridium, Inc. 2012-2025 */
+    /*@ $com.tridiumuniversity.checksumService.BChecksumService(21973052)1.0$ @*/
+    /* Generated Fri Apr 25 15:10:44 BST 2025 by Slot-o-Matic (c) Tridium, Inc. 2012-2025 */
 
-  //region Property "checksum"
+    //region Property "checksum"
 
-  /**
-   * Slot for the {@code checksum} property.
-   * @see #getChecksum
-   * @see #setChecksum
-   */
-  public static final Property checksum = newProperty(0, "", null);
+    /**
+     * Slot for the {@code checksum} property.
+     * @see #getChecksum
+     * @see #setChecksum
+     */
+    public static final Property checksum = newProperty(0, "", null);
 
-  /**
-   * Get the {@code checksum} property.
-   * @see #checksum
-   */
-  public String getChecksum() { return getString(checksum); }
+    /**
+     * Get the {@code checksum} property.
+     * @see #checksum
+     */
+    public String getChecksum() { return getString(checksum); }
 
-  /**
-   * Set the {@code checksum} property.
-   * @see #checksum
-   */
-  public void setChecksum(String v) { setString(checksum, v, null); }
+    /**
+     * Set the {@code checksum} property.
+     * @see #checksum
+     */
+    public void setChecksum(String v) { setString(checksum, v, null); }
 
-  //endregion Property "checksum"
+    //endregion Property "checksum"
 
-  //region Action "generateChecksum"
+    //region Property "trigger"
 
-  /**
-   * Slot for the {@code generateChecksum} action.
-   * @see #generateChecksum()
-   */
-  public static final Action generateChecksum = newAction(Flags.SUMMARY, null);
+    /**
+     * Slot for the {@code trigger} property.
+     * @see #getTrigger
+     * @see #setTrigger
+     */
+    public static final Property trigger = newProperty(0, new BTimeTrigger(), null);
 
-  /**
-   * Invoke the {@code generateChecksum} action.
-   * @see #generateChecksum
-   */
-  public void generateChecksum() { invoke(generateChecksum, null, null); }
+    /**
+     * Get the {@code trigger} property.
+     * @see #trigger
+     */
+    public BTimeTrigger getTrigger() { return (BTimeTrigger)get(trigger); }
 
-  //endregion Action "generateChecksum"
+    /**
+     * Set the {@code trigger} property.
+     * @see #trigger
+     */
+    public void setTrigger(BTimeTrigger v) { set(trigger, v, null); }
 
-  //region Type
+    //endregion Property "trigger"
+
+    //region Action "generateChecksum"
+
+    /**
+     * Slot for the {@code generateChecksum} action.
+     * @see #generateChecksum()
+     */
+    public static final Action generateChecksum = newAction(Flags.HIDDEN, null);
+
+    /**
+     * Invoke the {@code generateChecksum} action.
+     * @see #generateChecksum
+     */
+    public void generateChecksum() { invoke(generateChecksum, null, null); }
+
+    //endregion Action "generateChecksum"
+
+    //region Type
 
     @Override
     public Type getType() { return TYPE; }
     public static final Type TYPE = Sys.loadType(BChecksumSvc.class);
 
-  //endregion Type
+    //endregion Type
 
 //@formatter:on
 //endregion /*+ ------------ END BAJA AUTO GENERATED CODE -------------- +*/
-
-    Logger log = Logger.getLogger("Checksum Generator");
-
+    Logger log = Logger.getLogger("Checksum Service");
+    private ExecutorService executor;
+    String ords;
     @Override
+    public void started() {
+        if (this.get("link") == null) {
+            BLink link = new BLink(getTrigger().getHandleOrd(),"fireTrigger","generateChecksum",true);
+            this.add("link", link);
+            log.info("added link "+ link);
+        }
+        executor = newSingleThreadExecutor();
+    }
+
     public Type[] getServiceTypes() {
         return new Type[]{BChecksumSvc.TYPE};
     }
-    public void onGenerateChecksum(Context cx) throws Exception {
 
-    }
-/*
-    public void onGenerateChecksum(Context cx) throws Exception {
-        BComponent parent = Sys.getStation();
-
-        stationRecurse(parent);
-    }
-
-
-    public void stationRecurse(BComponent item) throws Exception {
-        String allOrds ="";
-        BComponent[] children = item.getChildComponents();
-        for (BComponent child : children) {
-            Property[] slots = child.getPropertiesArray();
-            for (Slot slot : slots) {
-                allOrds += child.getOrdInSession().toString();
-
-            }
-            stationRecurse(child);
-        }
-        setChecksum(generateChecksum(allOrds));
-    }
-
-    public String generateChecksum(String ords) throws Exception {
-        try {
-            String check = Utils.hex(Utils.sha256(ords.getBytes(StandardCharsets.UTF_8)));
-            log.info(check);
-            return check;
-
-        } catch (Exception Ex) {
-
-            log.info("Invalid data received - " + Ex);
-            return "";
+    public void doGenerateChecksum() {
+        if (this.getEnabled()) {
+            executor.execute(this::createChecksum);
         }
     }
 
- */
+    private void createChecksum() {
+        Checksum crc32 = new CRC32();
+        calcChecksum(Sys.getStation(), crc32);
+        long checksum = crc32.getValue();
+        setChecksum(Long.toHexString(checksum));
+    }
+
+
+    private void calcChecksum(BComponent comp,Checksum checks) {
+        checks.update(comp.getHandleOrd().toString().getBytes(),0,comp.getHandleOrd().toString().length());
+        BComponent[] childComponents = comp.getChildComponents();
+        for (int i = 0; i < childComponents.length; i++) {
+            calcChecksum(childComponents[i],checks);
+        }
+    }
+
+
+    @Override
+    public void stopped() {
+        if (executor != null) {
+            executor.shutdownNow();
+        }
+    }
+
+
+
 }
